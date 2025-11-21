ee902d51cbe28abfff4c82e2ee654fc403120802
ietsnut <marijn@mulholland.ai>
2025-11-21 19:56:05 +0100

    mac compat

--- core/src/main/java/com/trinketier/CoreLauncher.java
+++ core/src/main/java/com/trinketier/CoreLauncher.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
 import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.glutils.HdpiUtils;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Plane;
 import com.badlogic.gdx.math.Vector3;
@@ -125,7 +126,10 @@ public class CoreLauncher extends ApplicationAdapter {
         billboard.update(camera);
 
         // --- Rendering ---
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        HdpiUtils.glViewport(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getHeight());
+        //Gdx.gl.glViewport(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getHeight());
+        System.out.println("width: " + Gdx.graphics.getWidth());
+        System.out.println("height: " + Gdx.graphics.getHeight());
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.15f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
 
