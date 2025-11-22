b1f85881d2804f248d8186ee3fb446c7484fd306
ietsnut <marijnbrussel@hotmail.com>
2025-11-22 21:18:47 +0100

    gonna redo it now

---
+++ xcode/Trinketier/Trinketier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trinketier
+//
+//  Created by Marijn Brussel on 22/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcode/Trinketier/Trinketier/TrinketierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinketierApp.swift
+//  Trinketier
+//
+//  Created by Marijn Brussel on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrinketierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
